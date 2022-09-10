c680618a40e16a9bc0300b009ffb001a4503560b
Edinson Duran <eduran@condorlabs.io>
2022-09-10 17:08:49 -0500

    add docker-compose

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-require('dotenv').config({ path: '../.env' })
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const { HOST, PORT } = process.env
 
-// middleware
+//  middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
