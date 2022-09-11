c67316c63dba861c0ed9ed36b64897e7e484fe0e
Edinson Duran <eduran@condorlabs.io>
2022-09-10 22:43:54 -0500

    add test integration

---
+++ src/repositories/auth.test.js
@@ -0,0 +1,59 @@
+const repo = require('./auth');
+const authModel = require('../database/models/auth');
+
+jest.mock('../database/models/auth', () => {
+  return {
+    create: jest.fn().mockReturnThis(),
+    scan: jest.fn().mockReturnThis(),
+    contains: jest.fn().mockReturnThis(),
+    exec: jest.fn().mockReturnThis(),
+  };
+});
+
+const validAuth = {
+  endpoint: "/api/example",
+  body: {
+    example: ":Wenas:"
+  },
+}
+
+describe('When the function getLogout runs', () => {
+  it('should throw an error if no id is passed', async () => {
+    let result = null;
+    try {
+      result = await repo.getLogout();
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(authModel.exec).toHaveBeenCalledTimes(0);
+      expect(result).toBe(null);
+    }
+    expect(() => {
+      throw Error();
+    }).toThrow();
+  });
+});
+
+describe('When the function postLogout runs', () => {
+  it('should call to the table if the data are correct', async () => {
+    const authSpy = jest.spyOn(authModel, 'create').mockReturnValue(validAuth);
+    const result = await repo.postLogout(validAuth);
+    expect(authSpy).toHaveBeenCalledTimes(1);
+    expect(result).toBe(validAuth);
+    authSpy.mockRestore();
+  });
+
+  it('should throw an error if no object is passed', async () => {
+    let result = null;
+    const authSpy = jest.spyOn(authModel, 'create');
+    try {
+      result = await repo.postLogout();
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(authSpy).toHaveBeenCalledTimes(0);
+      expect(result).toBe(null);
+    }
+  }
+  );
+});
